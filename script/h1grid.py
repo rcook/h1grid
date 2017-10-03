46c73e43339ed2f727994d5df20ade42fb3222c5
Richard Cook <rcook@rcook.org>
2017-10-03 10:55:55 -0700

    Read API key from command line or environment

--- script/h1grid.py
+++ script/h1grid.py
@@ -4,9 +4,25 @@
 ##################################################
 
 from __future__ import print_function
+import argparse
+
+from h1gridlib.env import EnvDefault
+
+def _main_inner(args):
+    print("Hello from h1grid")
+    print(args)
 
 def _main():
-    print("Hello from h1grid")
+    parser = argparse.ArgumentParser(description="h1grid tool")
+    parser.add_argument(
+        "--api-key",
+        "-k",
+        metavar="APIKEY",
+        action=EnvDefault,
+        env_var="H1GRID_API_KEY",
+        help="API key")
+    args = parser.parse_args()
+    _main_inner(args)
 
 if __name__ == "__main__":
     _main()
