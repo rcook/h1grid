ee0c605ca3a6e0cecaef98c95e0055243040d112
Richard Cook <rcook@rcook.org>
2017-10-06 21:45:08 -0700

    Rename field

--- script/h1gridlib/component_info.py
+++ script/h1gridlib/component_info.py
@@ -7,18 +7,18 @@ from pyprelude.util import unpack_args
 _API_STORAGE_PATH = "api/storage"
 
 class ComponentInfo(object):
-    def __init__(self, api_key, artifactory_url, real_component_path, virtual_component_path=None):
+    def __init__(self, api_key, artifactory_url, component_path, virtual_component_path=None):
         self._api_key = api_key
         self._artifactory_url = artifactory_url.strip("/")
-        self._real_component_path = real_component_path.strip("/")
-        temp = real_component_path if virtual_component_path is None else virtual_component_path
+        self._component_path = component_path.strip("/")
+        temp = component_path if virtual_component_path is None else virtual_component_path
         self._virtual_component_path = temp.strip("/")
 
     @property
     def api_key(self): return self._api_key
 
     def api_url(self, *args):
-        return self._make_api_url(self._real_component_path, *args)
+        return self._make_api_url(self._component_path, *args)
 
     def virtual_api_url(self, *args):
         return self._make_api_url(self._virtual_component_path, *args)
